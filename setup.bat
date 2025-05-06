fcd2a9c65a34ceeac73ea52471dc7457fb1ab32e
fayamonkey <fayamonkeyrecords@gmail.com>
2025-05-06 16:05:21 +0200

    Add setup.bat

---
+++ setup.bat
@@ -0,0 +1,7 @@
+@echo on 
+echo Installiere benötigte Pakete für Eberle Artikel-Matching... 
+python -m pip install streamlit pandas xlsxwriter openpyxl 
+echo. 
+echo Installation abgeschlossen 
+echo Sie können die Anwendung nun mit start.bat starten. 
+pause 
